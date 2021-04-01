415f2b05cbdb66948ee6660471977584c9d03b49
joao.silva <joao.silva@arapiraca.al.gov.br>
2021-03-31 21:41:11 -0300

    Adicionado vetur config

---
+++ vetur.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  settings: {
+    "vetur.useWorkspaceDependencies": true,
+    "vetur.experimental.templateInterpolationService": true
+  },
+
+  projects: [
+    {
+      root: './frontend/banco_django_vuejs_frontend'
+    }
+  ]
+}
