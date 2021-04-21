9152bc11e55903a58acf10cc009b803b6835056d
joao.silva <joao.silva@arapiraca.al.gov.br>
2021-04-20 23:26:59 -0300

    Componente form para ediçao e inclusao de bancos

---
+++ frontend/banco_django_vuejs_frontend/components/page_components/FormBanco.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card" @input="getFormData">
+    <div class="card-header">
+      <p class="card-header-title">
+        Criar novo banco
+      </p>
+    </div>
+    <div class="card-content">
+      <b-field label="Código do banco">
+        <b-input v-model="banco.codigo_banco" name="codigo_banco"></b-input>
+      </b-field>
+      <b-field label="Nome">
+        <b-input v-model="banco.nome" name="nome"></b-input>
+      </b-field>
+    </div>
+    <footer class="card-footer is-justify-content-center pt-3 pb-3">
+      <div class="buttons">
+        <b-button type="is-primary" @click="salvarEvent">Salvar</b-button>
+        <b-button @click="cancelarEvent">Cancelar</b-button>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Emit } from 'vue-property-decorator'
+import Banco from '~/types/Banco'
+
+@Component
+export default class FormBanco extends Vue {
+
+  banco: Banco = {
+    codigo_banco: '',
+    nome: ''
+  }
+
+  @Emit('input')
+  getFormData ():Banco {
+    return this.banco
+  }
+
+  salvarEvent():void {
+    this.$parent.submit()
+  }
+
+  cancelarEvent():void {
+    this.$router.push('/bancos')
+  }
+
+}
+
+</script>
--- frontend/banco_django_vuejs_frontend/pages/bancos/create.vue
+++ frontend/banco_django_vuejs_frontend/pages/bancos/create.vue
@@ -4,15 +4,35 @@
       <li><nuxt-link to="/bancos">Bancos</nuxt-link></li>
       <li class="is-active"><a href="#" aria-current="page">Criar novo banco</a></li>
     </breadcrumb>
+    <div class="columns">
+      <div class="column is-6">
+        <form-banco v-model="formData" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-import Breadcrumb from '~/components/Breadcrumb'
+import Breadcrumb from '~/components/base_components/Breadcrumb.vue'
+import FormBanco from '~/components/page_components/FormBanco.vue'
+import Banco from '~/types/Banco'
 
-@Component
+@Component({
+  components: {
+    Breadcrumb, FormBanco
+  }
+})
 export default class BancosCreate extends Vue {
+
+  formData: Banco = {
+    codigo_banco: '',
+    nome: ''
+  }
+
+  submit ():void {
+    window.console.log(this.formData)
+  }
 
 }
 
